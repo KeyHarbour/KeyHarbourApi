39c1145ed5e8b9ea00124523988770960f39f026
Denis Fabien <denis@changendevops.com>
2026-09-14 14:37:04 -0400

    fix: Pipeline

--- spec/dummy/db/logs_schema.rb
+++ spec/dummy/db/logs_schema.rb
@@ -10,8 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_19_120636) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
+  create_table "action_logs", force: :cascade do |t|
+    t.string "action"
+    t.bigint "action_id"
+    t.string "controller"
+    t.datetime "created_at", null: false
+    t.string "method_type"
+    t.bigint "organization_id"
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+  end
+
+  create_table "api_queries", force: :cascade do |t|
+    t.string "action", null: false
+    t.string "controller", null: false
+    t.datetime "created_at", null: false
+    t.datetime "date", default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.string "service_name"
+    t.bigint "serviceable_id", null: false
+    t.string "serviceable_type", null: false
+    t.integer "token"
+    t.datetime "updated_at", null: false
+    t.index ["date"], name: "index_api_queries_on_date"
+    t.index ["service_name"], name: "index_api_queries_on_service_name"
+  end
 end
